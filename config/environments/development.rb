8467dffc79c2d2df718c7c3f83eb3c31516cb43e
Megan Street <meganjstreet@gmail.com>
2024-10-15 11:14:14 +0200

    moving credentials to env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'example.com',
-    user_name:            'norburj@gmail.com',
-    password:             'uexj piha xhtf xhxf',
+    user_name: ENV['SMTP_USERNAME'],
+  password: ENV['SMTP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
